fb179320389f6f936e49d7ace8b11430cb6a7517
gabrielrandrade <gabrielrandradesp@hotmail.com>
2024-11-07 11:39:38 -0300

    '#'

--- app_react-native_tii07/screens/detailsScreens.tsx
+++ app_react-native_tii07/screens/detailsScreens.tsx
@@ -27,7 +27,7 @@ class DetailScreen extends Component{
                     Lorem Ipsum is simply dummy text of the printing and 
                     typesetting industry. Lorem Ipsum has been the industry's standard 
                     dummy text ever since the 1500s, when an unknown printer 
-                    took a galley of type and scrambled it to make a type specimen book.
+                    took a galley of type and scrambled it to make a type specimen bookk.
                 </Text>
             </View>
         );
